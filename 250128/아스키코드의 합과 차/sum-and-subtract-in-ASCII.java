1607360026852c8b25a67a26a05fecf1ff6e2f0d
Dayun417 <leedayun03417@gmail.com>
2025-01-28 20:56:27 +0900

    Title: 아스키코드의 합과 차, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

--- 250128/아스키코드의 합과 차/sum-and-subtract-in-ASCII.java
+++ 250128/아스키코드의 합과 차/sum-and-subtract-in-ASCII.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.print((int)'L' + (int)'e' + " ");
-        System.out.println((int)'e' - (int)'L');
+        Scanner scanner = new Scanner(System.in);
+
+        char char1 = scanner.next().charAt(0);
+        char char2 = scanner.next().charAt(0);
+
+        int sum = (int) char1 + (int) char2;
+        int diff = Math.abs((int) char1 - (int) char2);
+
+        System.out.print(sum + " ");
+        System.out.println(diff);
+
+        scanner.close();
     }
 }
