5039f577e82bd33380dad8faa6c911c8850e2710
Dayun417 <leedayun03417@gmail.com>
2025-04-30 22:02:24 +0900

    Title: 세 정수형 변수 선언, Time: 198ms, Memory: 14MB, Status: Passed - Codetree

--- 250430/세 정수형 변수 선언/declaration-of-three-natural-numbers.js
+++ 250430/세 정수형 변수 선언/declaration-of-three-natural-numbers.js
@@ -3,8 +3,4 @@ let a = 7;
 let b = 23;
 let c = 30;
 
-let temp = a + b;
-let result = temp + c;
-
-console.log(`${a} + ${b} = ${temp}`);
-console.log(`${temp} + ${c} = ${result}`);
+console.log(`${a} + ${b} = ${c}`);
