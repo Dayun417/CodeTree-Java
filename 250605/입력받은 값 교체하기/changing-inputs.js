81f4ec7671402d7fcb93fe1124616f39faeb71ed
Dayun417 <leedayun03417@gmail.com>
2025-06-05 15:14:40 +0900

    Title: 입력받은 값 교체하기, Time: 164ms, Memory: 14MB, Status: Wrong Answer - Codetree

--- 250605/입력받은 값 교체하기/changing-inputs.js
+++ 250605/입력받은 값 교체하기/changing-inputs.js
@@ -1,14 +1,6 @@
 // 입력값
-let A = 1;
-let B = 2;
-let C = 3;
+let a = 75;
+let b = 24;
 
-// 처리 로직 (예시로 A와 B를 교환하고, C를 그대로 출력)
-let temp = A;
-A = B;
-B = temp;
-
-// 결과 출력
-console.log(A);
-console.log(B);
-console.log(C);
+// 순서 바꿔서 출력
+console.log(b, a);
